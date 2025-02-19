f9205eeb72bff355a6a7e6eb5467064d4e287b67
Yuting Zhang <3051803563@qq.com>
2025-02-19 22:02:31 +0800

    The racecar model and corresponding controller configuration have been changed.

--- racecar_sim/racecar_description/scripts/servo_commands_twist.py
+++ racecar_sim/racecar_description/scripts/servo_commands_twist.py
@@ -26,73 +26,34 @@ def limsteer(data, maxdata):
  
 # 接受 ackermann_msgs/AckermannDriveStamped 类型的消息 控制 阿克曼小车模型四个轮子的速度和前轮转角
 def set_throttle_steer(data):
-    global L, B_f, B_r, max_rad
- 
+
     pub_vel_left_rear_wheel = rospy.Publisher('/racecar/left_rear_wheel_velocity_controller/command', Float64, queue_size=1)
     pub_vel_right_rear_wheel = rospy.Publisher('/racecar/right_rear_wheel_velocity_controller/command', Float64, queue_size=1)
     pub_vel_left_front_wheel = rospy.Publisher('/racecar/left_front_wheel_velocity_controller/command', Float64, queue_size=1)
     pub_vel_right_front_wheel = rospy.Publisher('/racecar/right_front_wheel_velocity_controller/command', Float64, queue_size=1)
- 
+
     pub_pos_left_steering_hinge = rospy.Publisher('/racecar/left_steering_hinge_position_controller/command', Float64, queue_size=1)
     pub_pos_right_steering_hinge = rospy.Publisher('/racecar/right_steering_hinge_position_controller/command', Float64, queue_size=1)
- 
-    v = data.drive.speed                       # 速度 (m/s)
-    rad = data.drive.steering_angle            # 角度 (rad)
- 
-    if v != 0 and rad != 0:
-        # r 为理想的中间轮胎的转弯半径
-        r = L / math.tan(rad)
-        w = v / r
- 
-        # math.copysign(x, y)：返回一个与 x 数值大小相同、与 y 符号相同的浮点数
-        rL_rear = r - (math.copysign(1, w) * (B_r/2.0)) 
-        rR_rear = r + (math.copysign(1, w) * (B_r/2.0))
- 
-        L_front_temp = r - (math.copysign(1, w) * (B_f/2.0)) 
-        R_front_temp = r + (math.copysign(1, w) * (B_f/2.0))
-        rL_front = math.sqrt(math.pow(L_front_temp, 2) + math.pow(L, 2))
-        rR_front = math.sqrt(math.pow(R_front_temp, 2) + math.pow(L, 2))
- 
-        # 速度, m/s
-        vL_rear = v * rL_rear/r
-        vR_rear = v * rR_rear/r
-        vL_front = v * rL_front/r
-        vR_front = v * rR_front/r
- 
-        # 角度, rad
-        anL_front = math.atan2(L, L_front_temp) * math.copysign(1, w)
-        anR_front = math.atan2(L, R_front_temp) * math.copysign(1, w)
- 
-    else:
-        # 速度
-        vL_rear = v
-        vR_rear = v
-        vL_front = v
-        vR_front = v
- 
-        # 角度
-        anL_front = rad
-        anR_front = rad 
- 
-    anL_front = limsteer(anL_front, max_rad)  
-    anR_front = limsteer(anR_front, max_rad)
- 
-    pub_vel_left_rear_wheel.publish(vL_rear)
-    pub_vel_right_rear_wheel.publish(vR_rear)
-    pub_vel_left_front_wheel.publish(vL_front)
-    pub_vel_right_front_wheel.publish(vR_front)
-    pub_pos_left_steering_hinge.publish(anL_front)
-    pub_pos_right_steering_hinge.publish(anR_front)
+
+    throttle = data.linear.x*13.95348
+    steer = data.angular.z
+
+    pub_vel_left_rear_wheel.publish(throttle)
+    pub_vel_right_rear_wheel.publish(throttle)
+    pub_vel_left_front_wheel.publish(throttle)
+    pub_vel_right_front_wheel.publish(throttle)
+    pub_pos_left_steering_hinge.publish(steer)
+    pub_pos_right_steering_hinge.publish(steer)
  
     # 表格形式输出
     print(f"{'Wheel/Steering':<25}{'Value':<10}")
     print("-" * 35)
-    print(f"{'Left Front Wheel Velocity : ':<25}{vL_front:.2f} m/s")
-    print(f"{'Right Front Wheel Velocity: ':<25}{vR_front:.2f} m/s")
-    print(f"{'Left Rear Wheel Velocity  : ' :<25}{vL_rear:.2f} m/s")
-    print(f"{'Right Rear Wheel Velocity : ':<25}{vR_rear:.2f} m/s")
-    print(f"{'Left Front Steering Angle : ':<25}{anL_front:.2f} rad")
-    print(f"{'Right Front Steering Angle: ':<25}{anR_front:.2f} rad")
+    print(f"{'Left Front Wheel Velocity : ':<25}{throttle:.2f} m/s")
+    print(f"{'Right Front Wheel Velocity: ':<25}{throttle:.2f} m/s")
+    print(f"{'Left Rear Wheel Velocity  : ' :<25}{throttle:.2f} m/s")
+    print(f"{'Right Rear Wheel Velocity : ':<25}{throttle:.2f} m/s")
+    print(f"{'Left Front Steering Angle : ':<25}{steer:.2f} rad")
+    print(f"{'Right Front Steering Angle: ':<25}{steer:.2f} rad")
  
  
 # 将 geometry_msgs/Twist 类型的消息 转换为 阿克曼小车模型四个轮子的速度和前轮转角
@@ -169,7 +130,7 @@ def servo_commands():
     rospy.init_node('servo_commands', anonymous=True)
  
     # rospy.Subscriber("/ackermann_cmd", AckermannDriveStamped, set_throttle_steer) 
-    rospy.Subscriber("/cmd_vel", Twist, set_speed)
+    rospy.Subscriber("/vel_and_steering", Twist, set_throttle_steer)
  
     rospy.spin()
  
